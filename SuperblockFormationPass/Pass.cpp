7eec81306d7193dc880f88b3a9974b684d8236d9
Meredith Benson <merbenso@umich.edu>
2023-11-29 21:13:40 -0500

    get BFS list of BBs in loop, write out trace logic

--- SuperblockFormationPass/Pass.cpp
+++ SuperblockFormationPass/Pass.cpp
@@ -30,12 +30,12 @@ namespace {
 struct SuperblockFormationPass : public PassInfoMixin<SuperblockFormationPass> {
 
     PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-        llvm::BlockFrequencyAnalysis::Result &bfi = FAM.getResult<BlockFrequencyAnalysis>(F);
-        llvm::BranchProbabilityAnalysis::Result &bpi = FAM.getResult<BranchProbabilityAnalysis>(F);
+        // llvm::BlockFrequencyAnalysis::Result &bfi = FAM.getResult<BlockFrequencyAnalysis>(F);
+        // llvm::BranchProbabilityAnalysis::Result &bpi = FAM.getResult<BranchProbabilityAnalysis>(F);
         llvm::LoopAnalysis::Result &li = FAM.getResult<LoopAnalysis>(F);
         //llvm::LoopNestAnalysis::Result &lni = FAM.getResult<LoopNestAnalysis>(F);
         
-
+        // ------------------------------------------ identifying loops ---------------------------------------------------------
         // set up the lists and initialize them with top level loops in program
         std::list<Loop*> bfs_loops;
         std::list<Loop*> least_to_most_nested;
@@ -65,7 +65,28 @@ struct SuperblockFormationPass : public PassInfoMixin<SuperblockFormationPass> {
             errs() << "Loop depth: " << temp_loop->getLoopDepth() << "\n";
         }
 
+        // ------------------------------------------ trace formation ---------------------------------------------------------
+
         //to iterate through the list in most-nested order, access the back element of list and then pop it off.
+            //for each loop, get BFS list of basic blocks in loop
+            while(!least_to_most_nested.empty()){
+                Loop* current_loop = least_to_most_nested.back();
+                least_to_most_nested.pop_back();
+                 
+                std::vector<BasicBlock*>bfs_blocks = current_loop->getBlocksVector(); //this gets the blocks in breadth-first order!
+                errs() << "This is a loop!" << "\n";
+                for (BasicBlock* temp_block : bfs_blocks){
+                    errs() << "Basic Block: " << *temp_block << "\n";
+                }
+
+                //iterate thru list of basic blocks, inserting them into new list of visited blocks
+                // add current block to visited, then check if it has an indirect jump or a subroutine return. Stop trace if so. 
+                // if not, get the successor block of the current block. If more than one, use heuristics to choose.
+                // once you have the likely block, check if it is in visited. Stop trace if so.
+                // if likely block dominates current block, stop growing trace. 
+                // If made it this far, add the likely block to the trace and set the likely block to be new current block!
+            }
+
 
 
         // This only gets each top level loop. I then need to use LoopNest class to get each nested loop. 
@@ -76,14 +97,6 @@ struct SuperblockFormationPass : public PassInfoMixin<SuperblockFormationPass> {
         // BasicBlock *latch = L->getLoopLatch();
         // BasicBlock *preheader = L->getLoopPreheader();
 
-        // //std::unique_ptr<LoopNest>tempNest = &L->getLoopNest();
-        // errs() << "Loop nest object: " << *L << "\n";
-        // errs() << "Loop nest header: " << *header << "\n";
-        
-        // for (Loop* loop_obj : *L){
-        //     errs() << "Loop object: " << *loop_obj << "\n";
-        //     errs() << "Loop header: " << *loop_obj->getHeader() << "\n";
-        // }
         
       // Your pass is modifying the source code. Figure out which analyses are preserved and only return those, not all.
       return PreservedAnalyses::all();
