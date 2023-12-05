761ff78d60e1434652d951e502098d7012b6b37e
Christina Lilly Deng <chrdeng@eecs583a.eecs.umich.edu>
2023-12-05 04:29:34 -0500

    trying to figure out how to compare value and string.

--- SuperblockFormationPass/Pass.cpp
+++ SuperblockFormationPass/Pass.cpp
@@ -17,12 +17,71 @@
 #include "llvm/Transforms/Utils/LoopUtils.h"
 #include "llvm/IR/Use.h"
 #include "llvm/IR/User.h"
+#include "llvm/IR/InstrTypes.h"
 
 #include <iostream>
 
 using namespace llvm;
-
+using namespace std;
 namespace {
+
+//Get the ICmpInst from Instruction
+/*
+ICmpInst *ICC = dyn_cast<ICmpInst>(&I);
+llvm::CmpInst::Predicate pr=ICC->getSignedPredicate();
+switch(pr){
+    case CmpInst::ICMP_SGT: errs()<<"------>SGT\n"; break;
+    case CmpInst::ICMP_SLT: errs()<<"------>SLT\n"; break; 
+    case CmpInst::ICMP_SGE: errs()<<"------>SGE\n"; break; 
+    case CmpInst::ICMP_SLE: errs()<<"------>SLE\n"; break;
+}
+*/
+
+//returns true if the predicate is SLT
+bool isSLT(CmpInst *cmpInst) {
+    if (cmpInst->getPredicate() == CmpInst::ICMP_SLT) {
+        return true;
+    }
+    return false;
+}
+
+bool isUsedByBranch(Instruction &I) {
+    for (User *U : I.users()) {
+        errs() << "User *U: " << *U << "\n";
+        auto userInstr = dyn_cast<Instruction>(U);
+        string userOpcode = userInstr -> getOpcodeName();
+        if (userOpcode == "br") {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isZero(Instruction &I) {
+    if (I.getNumOperands() > 0) {
+        Value &v = *I.getOperand(1);
+        errs() << "v:" << 1 << " " << v << "\n";
+        //errs() << *v.getType();
+
+        auto output = dyn_cast<Instruction>(&v);
+
+
+        return true;
+    }
+    return false;
+}
+
+void opcode_heuristic(BasicBlock &BB) {
+    for (Instruction &I : BB) {
+        if (CmpInst *cmpInst = dyn_cast<CmpInst>(&I)) {
+            errs() << "I" << I << "\n";
+            isZero(I);
+            isUsedByBranch(I);
+            isSLT(cmpInst);
+            
+        }
+    }
+}
 
 struct SuperblockFormationPass : public PassInfoMixin<SuperblockFormationPass> {
 
@@ -38,7 +97,13 @@ struct SuperblockFormationPass : public PassInfoMixin<SuperblockFormationPass> {
             BasicBlock *preheader = L->getLoopPreheader();
 
             errs() << "header of loop: " << header << "\n";
+
         }
+        for (BasicBlock &BB : F) {
+            opcode_heuristic(BB);
+        }
+        
+        
 
 
 
---
+++ benchmarks/christina/opcode_heuristic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int i, j;
+	j = 5;
+	for(i = 0; i < 10; i++) {
+		if (j < 0) 
+        {
+            j++;
+            printf("Hello World!");
+        }
+        else {
+            j--;
+            printf("Goodbye World!");
+        }
+	}
+	return 0;
+}
