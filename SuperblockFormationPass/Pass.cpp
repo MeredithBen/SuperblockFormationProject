919903e4a706ac1ac939c2110e5bb2ddc562180d
Agarwal <riyaag@umich.edu>
2023-12-10 11:57:02 -0500

    Added 3 heuristics and accuracy code

--- SuperblockFormationPass/Pass.cpp
+++ SuperblockFormationPass/Pass.cpp
@@ -18,29 +18,52 @@
 #include "llvm/IR/Use.h"
 #include "llvm/IR/User.h"
 #include "llvm/IR/InstrTypes.h"
-#include <vector>
-#include <unordered_map>
-#include <queue>
-
-
 
 #include <iostream>
 
 using namespace llvm;
 using namespace std;
+
+struct RelBranch {
+    BasicBlock *bb;
+    string opcode;
+    llvm::CmpInst::Predicate pr;
+    std::list<std::pair<llvm::Value* , llvm::Value* >> operandPair;
+    int heuristic;
+    bool dir;
+};
+
+std::vector<RelBranch> relbranch;
+
 namespace {
 
-//Get the ICmpInst from Instruction
-/*
-ICmpInst *ICC = dyn_cast<ICmpInst>(&I);
-llvm::CmpInst::Predicate pr=ICC->getSignedPredicate();
-switch(pr){
-    case CmpInst::ICMP_SGT: errs()<<"------>SGT\n"; break;
-    case CmpInst::ICMP_SLT: errs()<<"------>SLT\n"; break; 
-    case CmpInst::ICMP_SGE: errs()<<"------>SGE\n"; break; 
-    case CmpInst::ICMP_SLE: errs()<<"------>SLE\n"; break;
+void relatedBranchesHeuristic(BasicBlock* BB, string opc, llvm::CmpInst::Predicate pred, std::list<std::pair<Value* , Value* >> oppair, int heur, bool path) {
+    for (auto& branch : relbranch) {
+        if (oppair == branch.operandPair) {
+            if (heur > branch.heuristic) {
+                // change the direction of the new branch according to the higher priority heuristic
+                if (pred == branch.pr) {
+                    relbranch.push_back({BB, opc, pred, oppair, branch.heuristic, branch.dir});
+                } 
+                else {
+                    relbranch.push_back({BB, opc, pred, oppair, branch.heuristic, !branch.dir});
+                }
+                break;
+            }
+            else {
+                // if new branch statement has a higher priority heuristic, then need to update all other branches with same variables
+                branch.heuristic = heur;
+                if (pred == branch.pr) {
+                    branch.dir = path;
+                }
+                else {
+                    branch.dir = !path;
+                }
+            }
+        }
+    }
+    relbranch.push_back({BB, opc, pred, oppair, heur, path});
 }
-*/
 
 //returns true if the predicate is SLT
 bool isSLT(CmpInst *cmpInst) {
@@ -101,6 +124,7 @@ bool isNegativeComparison (Instruction &I) {
         switch(pr){
             case CmpInst::ICMP_SGT: return isZero(I, SGT);
             case CmpInst::ICMP_SLT: return isZero(I, SLT);
+            //case CmpInst::ICMP_EQ: return isFloatingPt(I, )
         }
     }
     return false;
@@ -119,80 +143,242 @@ void opcodeHeuristic(BasicBlock &BB) {
     }
 }
 
-//Gets all preds based on the current basic block
-unordered_map<BasicBlock *, BasicBlock *> createPredsMap(BasicBlock &BB, llvm::LoopAnalysis::Result &li) {
-    std::unordered_map<BasicBlock *, BasicBlock *> PredsMap;
-    std::queue<BasicBlock *> PredsQueue;
-    for (Loop *L : li) {
-        BasicBlock *currBB = L->getHeader();
-        BasicBlock *latch = L->getLoopLatch();
-        while (currBB != latch) {
-            for (BasicBlock *Pred : predecessors(currBB)) {
-                PredsQueue.push(Pred);
-                PredsMap[Pred] = currBB;
-            }
-            currBB = PredsQueue.front();
-            PredsQueue.pop();
-        }
-    }
-    return PredsMap;
+bool isPointerEqual (Instruction &I) {
+    // string userOpcode = I.getOpcodeName();
+    // if (userOpcode == "br" && isa<ICmpInst>(&I)) {
     
-}
-//Return true if found a backwards branch
-//Return false if only forwards
-bool branchDirectionHeuristic(BasicBlock &BB, llvm::LoopAnalysis::Result &li) {
-    std::unordered_map<BasicBlock *, BasicBlock *> PredsMap = createPredsMap(BB, li);
-    for (Instruction &I : BB) {
-        string opcode = I.getOpcodeName();
-        if (opcode == "br") {
-            for (unsigned i = 0; i < I.getNumOperands(); i++) {
-                BasicBlock *opBB = dyn_cast<BasicBlock>(I.getOperand(i));
-                if (PredsMap.find(opBB) != PredsMap.end()) {
-                    return true;
-                }
-            }
-        }
+    ICmpInst *ICC = dyn_cast<ICmpInst>(&I);
+    llvm::CmpInst::Predicate pr=ICC->getSignedPredicate();
+    errs() << "in pointer equal " << pr << "\n";
+    Value &op2 = *I.getOperand(1);
+    switch(pr){
+        case CmpInst::ICMP_EQ: return true;
+        break;
+        case CmpInst::ICMP_NE: return false;
+        break;
     }
     return false;
+    // }
 }
 
-// Finds store instruction
-// Gets the predecessor of that instruction
-// Gets the ICMP related to that predecessor's branch instruction
-// Gets the registers used in the ICMP
-// Checks if the registers were loaded with the same value as the store instruction
-// returns false if the same value was stored and loaded (The value was overwritten inside of the branch)
-// returns true if not.
-bool guardHeuristic(BasicBlock &BB) {
-    for (Instruction &Istore : BB) {
-        string opcode1 = Istore.getOpcodeName();
-        if (opcode1 == "store" && (Istore.getNumOperands() > 1)) {
-            Value &storeReg = *Istore.getOperand(1);
-            for (BasicBlock *Pred : predecessors(&BB)) {
-                for (Instruction &Icmp : *Pred) {
-                    Value &loadVal = *Icmp.getOperand(0);
-                    if (Instruction *loadInstr = dyn_cast<Instruction>(&loadVal)) {
-                        string opcode2 = loadInstr->getOpcodeName();
-                        if (isUsedByBranch(Icmp) && isa<ICmpInst>(&Icmp) && (opcode2 =="load") ) {
-                            for (unsigned i = 0; i < loadInstr->getNumOperands(); i++) {
-                                Value &loadReg = *loadInstr->getOperand(0);
-                                if (&loadReg == &storeReg) {
-                                    errs() << "loadReg" << loadReg << "\n";
-                                    errs() << "storeReg" << storeReg << "\n";
-                                    errs() << "Istore: " << Istore << "\n";
-                                    errs() << "loadVal: " << loadVal << "\n";
-                                    errs() << "Icmp: " << Icmp << "\n";
-                                    errs() << "Pred: " << *Pred << "\n";
-                                    return false;
-                                }
+int pointerHeuristic(BasicBlock &BB) {
+    for (Instruction &I : BB) {
+        string userOpcode = I.getOpcodeName();
+        // errs() << "in pointer, instr is" << I << "and opcode" << userOpcode << "\n";
+        if (userOpcode == "icmp") {
+            errs() << "the instr is " << I << "\n";
+            // auto temp = dyn_cast<Instruction>(I.getOperand(0));
+            auto I1 = dyn_cast<Instruction>(I.getOperand(0));
+            if (isa<LoadInst>(I1)) {
+                auto temp = dyn_cast<Instruction>(I1->getOperand(0));
+                string tempOpcode = temp->getOpcodeName();
+                if (tempOpcode == "getelementptr") {
+                    auto I2 = dyn_cast<Instruction>(I.getOperand(1));
+                    if (isa<LoadInst>(I2)) {
+                        auto temp2 = dyn_cast<Instruction>(I2->getOperand(0));
+                        string tempOpcode2 = temp2->getOpcodeName();
+                        if (tempOpcode2 == "getelementptr") {
+                            ICmpInst *ICC = dyn_cast<ICmpInst>(&I);
+                            llvm::CmpInst::Predicate pr=ICC->getSignedPredicate();
+                            llvm::Value* passop1 = I.getOperand(0);
+                            llvm::Value* passop2 = I.getOperand(1);
+                            std::list<std::pair<llvm::Value*, llvm::Value*>> oppair = {std::make_pair(passop1, passop2)};
+                            if (isPointerEqual(I)) {
+                                errs() << "Second label is taken (corresponding to else path)" << "\n";
+                                relatedBranchesHeuristic(&BB, userOpcode, pr, oppair, 1, false);
+                                return 2;
+                            }
+                            else {
+                                errs() << "First label is taken (corresponding to if path)" << "\n";
+                                relatedBranchesHeuristic(&BB, userOpcode, pr, oppair, 1, true);
+                                return 1;
                             }
                         }
                     }
+                    
                 }
+                else if (I.getOperand(1) == nullptr) {
+                    ICmpInst *ICC = dyn_cast<ICmpInst>(&I);
+                    llvm::CmpInst::Predicate pr=ICC->getSignedPredicate();
+                    Value* passop1 = I.getOperand(0);
+                    Value* passop2 = NULL;
+                    std::list<std::pair<llvm::Value*, llvm::Value*>> oppair = {std::make_pair(passop1, passop2)};
+                    switch(pr){
+                        case CmpInst::ICMP_EQ: errs() << "Second label is taken (corresponding to else path)" << "\n";
+                        relbranch.push_back({&BB, userOpcode, pr, oppair, 1, false});
+                        return 2;
+                        break;
+                        case CmpInst::ICMP_NE: errs() << "First label is taken (corresponding to if path)" << "\n";
+                        relbranch.push_back({&BB, userOpcode, pr, oppair, 1, true});
+                        return 1;
+                        break;
+                        default: errs() << "pointers have some other comparison operator" << "\n";
+                        return 0;
+                        break;
+                    }
+                }
+            }
+            // Value* op1 = temp->getOperand(0);
+            // Type* optype1 = op1->getType();
+            // errs() << "here1" << *op1 << "\n";
+            // if (optype1->isPointerTy()){
+            //     errs() <<"here2\n";
+            //     Value* op2 = I.getOperand(1);
+            //     Type* optype2 = op2->getType()->getContainedType(0);
+            //     if (optype2->isPointerTy()) {
+            //         if (isPointerEqual(I)) {
+            //             errs() << "Second label is taken (corresponding to else path)" << "\n";
+            //             return 2;
+            //         }
+            //         else {
+            //             errs() << "First label is taken (corresponding to if path)" << "\n";
+            //             return 1;
+            //         }
+            //     }
+            //     else if (op2 == nullptr) {
+            //         ICmpInst *ICC = dyn_cast<ICmpInst>(&I);
+            //         llvm::CmpInst::Predicate pr=ICC->getSignedPredicate();
+            //         switch(pr){
+            //             case CmpInst::ICMP_EQ: errs() << "Second label is taken (corresponding to else path)" << "\n";
+            //             return 2;
+            //             break;
+            //             case CmpInst::ICMP_NE: errs() << "First label is taken (corresponding to if path)" << "\n";
+            //             return 1;
+            //             break;
+            //             default: errs() << "pointers have some other comparison operator" << "\n";
+            //             return 0;
+            //             break;
+            //         }
+            //     }
+            // }
+        }        
+    }
+    errs() << "pointer heuristics are not used\n";
+    return 0;
+}
+
+int loopHeuristic(BasicBlock &BB, llvm::LoopAnalysis::Result &li) {
+    std::vector<llvm::BasicBlock *> loopHeaders;
+    int flag = 0;
+    for (Loop *L : li) {
+        BasicBlock *header = L->getHeader();
+        errs() << "loop header is " << *(header->getTerminator()->getSuccessor(0)) << "\n";
+        loopHeaders.push_back(header->getTerminator()->getSuccessor(0));
+    }
+    for (Instruction &I : BB) {
+        string userOpcode = I.getOpcodeName();
+        if (userOpcode == "br") {
+            for (BasicBlock *Succ: successors(&BB)) {
+                auto headercheck = std::find(loopHeaders.begin(), loopHeaders.end(), Succ);
+                if (headercheck != loopHeaders.end()) {
+                    errs() << "This block is taken" << *Succ << "\n";
+                    flag = 1;
+                    llvm::CmpInst::Predicate pr;
+                    Value* passop1 = NULL;
+                    Value* passop2 = NULL;
+                    std::list<std::pair<llvm::Value*, llvm::Value*>> oppair = {std::make_pair(passop1, passop2)};
+                    relbranch.push_back({&BB, userOpcode, pr, oppair, 2, true});
+                    return 1;
+                    // store(instruction/bb, predicate, opcode, variables, heuristic), could be a global var, or local passed from main func
+                }
+            }
+            
+        }
+    }
+    if (flag == 0) {
+        errs() << "loop heuristics not applied" << "\n";
+        return 0;
+    }
+    return 0;
+}
+
+void runHeuristics(Function &F, llvm::LoopAnalysis::Result &li) {
+    for (BasicBlock &BB : F) {
+        pointerHeuristic(BB);
+        loopHeuristic(BB, li);
+        opcodeHeuristic(BB);
+    }
+    errs() << relbranch[0].heuristic << "\n";
+        
+}
+
+BasicBlock* getMostLikely(BasicBlock* curr) {
+    for (auto& branch : relbranch) {
+        if (curr == branch.bb) {
+            bool path = branch.dir;
+            if (path) {
+                return curr->getTerminator()->getSuccessor(0);
+            }
+            else {
+                return curr->getTerminator()->getSuccessor(1);
             }
         }
     }
-    return true;
+    errs() << "couldn't find a successor, something wrong\n";
+    exit(0);
+}
+
+double getAccuracy(Function &F, llvm::BranchProbabilityAnalysis::Result &bpi, llvm::LoopAnalysis::Result &li) {
+    std::vector<llvm::BasicBlock *> freqPath;
+    for (Loop *L : li) {
+        BasicBlock *header = L->getHeader();
+        BasicBlock *latch = L->getLoopLatch();
+
+        BasicBlock *currblock = header;
+
+        while(currblock != latch){
+            freqPath.push_back(currblock);
+            for (BasicBlock *Succ: successors(currblock)) {
+                BranchProbability prob = bpi.getEdgeProbability(currblock, Succ);
+                uint64_t num = prob.getNumerator();
+                uint64_t den = prob.getDenominator();
+                double ratio = num/ static_cast<double>(den);
+                double comp = static_cast<double>(0.7999);
+                if (ratio >= comp) {
+                    currblock = Succ; //move to the next block on the frequent path
+                    break;
+                }
+            }
+        }
+        freqPath.push_back(latch);
+    }
+
+    BasicBlock* pred = nullptr;
+    double sa_acc = 0, pr_acc = 0;
+    for (auto BB : freqPath) {
+        
+        if (pred != nullptr) {
+            
+            Instruction* inst = pred->getTerminator();
+            string opc = inst->getOpcodeName();
+            BranchProbability prob = bpi.getEdgeProbability(pred, BB);
+            uint64_t num = prob.getNumerator();
+            uint64_t den = prob.getDenominator();
+            double ratio = num/ static_cast<double>(den);
+            pr_acc = pr_acc + ratio;
+            if (ratio == 1) {
+                sa_acc = sa_acc + ratio;
+            }
+            else {
+                BasicBlock* succ = getMostLikely(pred);
+                if (succ == BB) {
+                    sa_acc = sa_acc + ratio;
+                }
+                else {
+                    BranchProbability prob1 = bpi.getEdgeProbability(pred, succ);
+                    uint64_t num1 = prob.getNumerator();
+                    uint64_t den1 = prob.getDenominator();
+                    double ratio1 = num1/ static_cast<double>(den1);
+                    sa_acc = sa_acc + ratio1;
+                }
+            }
+            errs() << "here ratio" << ratio << "   " << *BB << "\n";
+        }
+        
+        pred = BB;
+
+    }
+    return sa_acc/pr_acc;
 }
 
 struct SuperblockFormationPass : public PassInfoMixin<SuperblockFormationPass> {
@@ -202,18 +388,12 @@ struct SuperblockFormationPass : public PassInfoMixin<SuperblockFormationPass> {
         llvm::BranchProbabilityAnalysis::Result &bpi = FAM.getResult<BranchProbabilityAnalysis>(F);
         llvm::LoopAnalysis::Result &li = FAM.getResult<LoopAnalysis>(F);
 
-        for (Loop *L : li) {
-            //This only gets each top level loop. I then need to use LoopNest class to get each nested loop. 
-            BasicBlock *header = L->getHeader();
-            BasicBlock *latch = L->getLoopLatch();
-            BasicBlock *preheader = L->getLoopPreheader();
-        }
-        for (BasicBlock &BB : F) {
-            guardHeuristic(BB);
+        runHeuristics(F, li);
+        double acc = getAccuracy(F, bpi, li);
 
-        }
-        
-        
+
+
+
       // Your pass is modifying the source code. Figure out which analyses are preserved and only return those, not all.
       return PreservedAnalyses::all();
     }
